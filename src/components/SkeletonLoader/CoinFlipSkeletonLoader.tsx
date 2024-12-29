56e9a265993f1090aea8ff9f784182a291d2a955
Amir <a.presovsky@list.ru>
2024-12-30 00:17:12 +0300

    added: type import

--- src/components/SkeletonLoader/CoinFlipSkeletonLoader.tsx
+++ src/components/SkeletonLoader/CoinFlipSkeletonLoader.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { type FC } from "react";
 import DesignCircles from "../DesignCircles/DesignCircles";
 import Footer from "../Footer";
 import styles from "./SkeletonLoader.module.css";
